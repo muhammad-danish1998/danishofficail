35463e3bf8db077b942127afe6579784e7482d5d
muhammad-danish1998 <iamdanish1998@gmail.com>
2022-10-07 21:56:15 +0500

    delete for site map

--- next-sitemap.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-// Default code you can customize according to your requirements.
-module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://www.danishtanoli.com/',
-    generateRobotsTxt: true, // (optional)
-    // REST CODE READ DOCS  ...
-    }
